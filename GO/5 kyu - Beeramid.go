51309108f5747e5cc13b4de65f21630dc824dbf2
0xkulikovd <kulikovdv@mstu.edu.ru>
2023-10-10 16:53:29 +0300

    5 kyu - Beeramid

---
+++ GO/5 kyu - Beeramid.go
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/51e04f6b544cf3f6550000c1
+
+package kata
+
+func Beeramid(bonus int, price float64) int {
+      if bonus = 0 { return 0 }
+  
+      cost = 0.0
+      level = 0
+      canCount = 0
+  
+       while loop
+      for cost = float64(bonus) {
+        level = level + 1
+        canCount = canCount + (level  level)
+        cost = float64(canCount)  price
+      }
+  
+      return level - 1
+}
